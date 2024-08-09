ad2cace728310a1c2101c09f37c03ed8a34ec145
root <root@DESKTOP-0RSBRDK>
2024-08-08 23:06:38 -0400

    session 2 FS112 MB

---
+++ pages/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render
+
+# # Create your views here.
